a339ceffb27b2828137e7ba3e0969eb693c18b37
竹上克司 <katudon12_27@i.softbank.jp>
2020-09-08 20:18:07 +0900

    FirstCommit
    ・docker-compose.ymlでdockerコンテナを記述[MySQL , Nginx , PHP]
    
    ・dockerディレクトリに[php][nginx]の環境設定ファイルを追加
    
    ・.envファイルを編集し、[データベース名：ApparelApp]と接続
    
    ・Auth認証でログイン機能を有効化
    >>Topページ、ログインページ、新規登録ページを日本語化
    
    ・Topページのヘッダーを編集
    >ログイン前は[ログイン][新規登録]
    >ログイン前は[ホーム]
    
    ・Topページ、ホームページのハンバーガーメニューを削除

---
+++ MyPortFolio/app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() /* [/home]へのアクセスを制限(ログイン後にアクセス可能) */
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
---
+++ MyPortFolio/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="home container-fuild bg-secondary">
+    <div class="row p-4">
+        <h1>ようこそ</h1>
+    </div>
+</div>
+@endsection
