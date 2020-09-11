10d5d4db4ebf5cc2ca45bac90d735210849cf920
竹上克司 <katudon12_27@i.softbank.jp>
2020-09-11 18:47:19 +0900

    secondCommit

--- MyPortFolio/resources/views/layouts/app.blade.php
+++ MyPortFolio/resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                <a class="dropdown-item" href="{{{{ route('logout') }}}}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     ログアウト
                                 </a>
