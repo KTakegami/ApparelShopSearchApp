a5f92fdee3550df6d2b2cc78152845593da2e8cf
竹上克司 <katudon12_27@i.softbank.jp>
2020-09-11 18:47:01 +0900

    ヘッダーテンプレートを継承しページを作成

--- MyPortFolio/resources/views/home.blade.php
+++ MyPortFolio/resources/views/home.blade.php
@@ -1,9 +1,23 @@
-@extends('layouts.app')
+@extends('layouts.header')
 
 @section('content')
-<div class="home container-fuild bg-secondary">
+<div class="home container-fuild bg-secondary" style="height: 600px">
     <div class="row p-4">
         <h1>ようこそ</h1>
     </div>
 </div>
+
+<footer class="container-fuild d-flex justify-content-center align-items-center       bg-light" style="height: 150px;">
+    <button class="btn btn-warning p-2">
+        <a href="{{ url('/') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="text-white">
+        <!-- フォームを発火 -->
+            ログアウト
+        </a>
+    </button>
+
+    <form id="logout-form" action="{{ url('/') }}" method="POST" class="d-none">
+        @csrf
+    </form>
+</footer>
+
 @endsection
