c28d01baf80ba7048e465baba43045b15ad16b6c
竹上克司 <katudon12_27@i.softbank.jp>
2020-10-14 12:22:15 +0900

    ・いいね機能のロジックを削除 ・画像の投稿/保存処理を修正

--- MyPortFolio/app/Http/Controllers/ShopController.php
+++ MyPortFolio/app/Http/Controllers/ShopController.php
@@ -77,15 +77,15 @@ class ShopController extends Controller
 
         $shop_image = $request->shop_image;
 
+        $filename = $shop_image->getClientOriginalName();
+        //画像のオリジナル名を取得
+        
         if($shop_image) { //画像投稿時public直下に画像を保存
-            $filePath = $shop_image->store('public');
-            //storeメソッドを呼ぶと、storage/app/public配下にデータが保存される
-
-            $shops->shop_image = str_replace('public', '',$filePath);
+            $path = $shop_image->storeAs('public', $filename); //$fileNameで保存する
+            $shops->shop_image = str_replace('public', '',$path);
         }
 
         $shops->save();
-
 
         return redirect('/shops');
     }
@@ -94,10 +94,7 @@ class ShopController extends Controller
         $user_id = Auth::id();
         $shop = Shop::findOrFail($id);
 
-        // $favorite = $shop->favorites()->where('user_id', Auth::user()->id)->first();
-
         return view('shops.show')->with('shop',$shop)->with('user_id',$user_id);
-        // ->with('favorite',$favorite);
     }
 
     public function edit($id) {
@@ -106,31 +103,38 @@ class ShopController extends Controller
         $prefs = config('prefectures');
         return view('shops.edit')->with('shops',$shops)->with('prefs', $prefs);
     }
-    
+
     public function update(Request $request,$id) {
         $shops = Shop::findOrFail($id);
+        $user = auth()->user();
 
-        logger()->info($shops->shop_image);
-        logger()->info($request->shop_image);
+        //ここから画像以外の処理
+        $shops->user_id = $user->id;
+        $shops->shop_name = $request->shopName;
+        $shops->shop_description = $request->shop_description;
+        $shops->genre_id = $request->genre;
+        $shops->product_id = $request->product;
+        $shops->prefecture_id = $request->pref_code;
+        $shops->shop_address = $request->address;
+        //ここまで画像以外の処理
 
         //ここから画像処理
-        // $reqImage = $request->shop_image;
+        $shop_image = $request->shop_image;
+        $filename = $shop_image->getClientOriginalName();
 
-        // if($reqImage) {
-        //     if(isset($shops->shop_image)) {
-        //         Storage::delete($shops->shop_image);
-        //         $filePath = $reqImage->store('public');
-        //         $shops->shop_image = str_replace('public','',$filePath);
-        //         $shops->save();
-        //     } else {
-        //         $filePath = $reqImage->store('public');
-        //         $shops->shop_image = str_replace('public', '', $filePath);
-        //         $shops->save();
-        //     }
-        // }
-        //ここまで
+        if($shop_image) {
+            if(isset($shops->shop_image)) {
+                Storage::delete('public' . $shops->shop_image); //public配下の画像を削除
+                $path = $shop_image->storeAs('public', $filename); //$fileNameで保存する
+                $shops->shop_image = str_replace('public', '', $path);
+            } else {
+                $path = $shop_image->storeAs('public', $filename); //$fileNameで保存する
+                $shops->shop_image = str_replace('public', '', $path);
+            }
+        }
+        //ここまで画像処理
 
-        $shops = Shop::findOrFail($id)->update($request->all());
+        $shops->save();
 
         return redirect('shops')->with('shops',$shops);
     }
