1a825ac05b319ef4a14fbac9237f891940de508f
竹上克司 <katudon12_27@i.softbank.jp>
2020-10-14 12:22:36 +0900

    いいね機能のロジックを削除

--- MyPortFolio/app/Favorite.php
+++ MyPortFolio/app/Favorite.php
@@ -3,24 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use kanazaca\CounterCache\CounterCache;
 
 class Favorite extends Model
 {
-  use CounterCache;
-  
-  public $counterCacheOptions = [
-    'Shop' => ['field' => 'favorites_count', 'foreignKey' => 'shop_id']
-  ];
 
-  protected $fillable = ['user_id', 'shop_id'];
-
-  public function Shop () {
-    return $this->belongsTo('App\Shop');
-  }
-  
-  public function User()
-  {
-    return $this->belongsTo(User::class);
-  }
 }
